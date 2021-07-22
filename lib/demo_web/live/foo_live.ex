0a36ce998fa84e129be26eeb1cedaf7d75d682f9
dvic <info@dvic.io>
2021-07-22 13:46:14 +0200

    Add alpine v3 x-transition and click.outside issue

---
+++ lib/demo_web/live/foo_live.ex
@@ -0,0 +1,9 @@
+defmodule DemoWeb.FooLive do
+  use DemoWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+end
--- lib/demo_web/router.ex
+++ lib/demo_web/router.ex
@@ -18,6 +18,7 @@ defmodule DemoWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/foo", FooLive, :index
   end
 
   # Other scopes may use custom stacks.
